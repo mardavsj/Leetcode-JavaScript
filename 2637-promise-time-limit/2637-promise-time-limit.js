b5d911c972b3449d7c95c444fe36878362d5fab1
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-05-16 13:55:58 +0530

    Time: 55 ms (84.04%), Space: 41.6 MB (91.12%) - LeetHub

--- 2637-promise-time-limit/2637-promise-time-limit.js
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -1,3 +1,8 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
 var timeLimit = function(fn, t) {
 	return async function(...args) {
         const timeout = new Promise((resolve, reject) => {
@@ -13,3 +18,8 @@ var timeLimit = function(fn, t) {
         }
     }
 };
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
