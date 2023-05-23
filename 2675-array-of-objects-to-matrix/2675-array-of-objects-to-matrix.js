0abe4347ba2d1ab5244ace071bf22329e4025232
Mardav Jadaun <mardavjadaun113@gmail.com>
2023-05-23 18:55:19 +0530

    Time: 544 ms (42.53%), Space: 112.7 MB (38.54%) - LeetHub

---
+++ 2675-array-of-objects-to-matrix/2675-array-of-objects-to-matrix.js
@@ -0,0 +1,30 @@
+const flattenBacktracking = (ele, path, object, columns) => {
+  if (ele != null && typeof ele == "object") {
+    Object.entries(ele).forEach(([key, value]) =>
+      flattenBacktracking(value, path + (path ? "." : "") + key, object, columns)
+    )
+  } else {
+    object[path] = ele
+    columns.add(path)
+  }
+  return object
+}
+var jsonToMatrix = function (arr) {
+  const matrix = []
+  const columns = new Set()
+
+  arr = arr.map((ele) => flattenBacktracking(ele, "", {}, columns))
+  matrix.push([...columns].sort())
+
+  const columnsIdx = matrix[0].reduce(
+    (acc, cur, idx) => ((acc[cur] = idx), acc),
+    {}
+  )
+
+  arr.forEach((ele) => {
+    matrix.push(Array(columns.size).fill(""))
+    Object.entries(ele).forEach(([key, value]) => (matrix.at(-1)[columnsIdx[key]] = value))
+  })
+
+  return matrix
+};
